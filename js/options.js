4c9bf23d3cad1e5a07ee8db541d4df30345fa75f
Drew Greene <drew@0ui.org>
2016-07-05 08:32:20 -0700

    convert some tabs to spaces

--- js/options.js
+++ js/options.js
@@ -1,25 +1,25 @@
-(function () {
+(function() {
 
-	let options = [
-		'open-tabs',
-		'auto-open',
-		'auto-play',
-		'loop-anim',
-	];
+  let options = [
+    'open-tabs',
+    'auto-open',
+    'auto-play',
+    'loop-anim',
+  ];
 
-	function restoreOptions() {
-		options.map((o) => {
-			$('#' + o).prop('checked', localStorage[o] === 'true');
-		});
-	}
+  function restoreOptions() {
+    options.map((o) => {
+      $('#' + o).prop('checked', localStorage[o] === 'true');
+    });
+  }
 
-	function saveOptions() {
-		options.map((o) => {
-			localStorage[o] = $('#' + o).is(':checked') ? 'true' : 'false';
-		});
-		$('#status').text('Options saved.').show().delay(700).fadeOut(400);
-	}
+  function saveOptions() {
+    options.map((o) => {
+      localStorage[o] = $('#' + o).is(':checked') ? 'true' : 'false';
+    });
+    $('#status').text('Options saved.').show().delay(700).fadeOut(400);
+  }
 
-	$('#save-button').on('click', saveOptions);
-	$(window).on('load', restoreOptions);
+  $('#save-button').on('click', saveOptions);
+  $(window).on('load', restoreOptions);
 })();
